1b0c62e3ca5de0c447e46010a2d0a596df6ed748
AngelNarang <102167920+AngelNarang@users.noreply.github.com>
2022-10-04 06:35:27 -0700

    Add files via upload

---
+++ PRO-C116-project-image-main-main/solar_planets.py
@@ -0,0 +1,13 @@
+import cv2
+img=cv2.imread("solar-system.jpg")
+cv2.putText(img,"Sun",(20,300),cv2.FONT_HERSHEY_COMPLEX,0.5,(255,255,255))
+cv2.putText(img,"Mercury",(80,280),cv2.FONT_HERSHEY_COMPLEX,0.5,(255,255,255))
+cv2.putText(img,"Venus",(180,280),cv2.FONT_HERSHEY_COMPLEX,0.5,(255,255,255))
+cv2.putText(img,"Earth",(280,280),cv2.FONT_HERSHEY_COMPLEX,0.5,(255,255,255))
+cv2.putText(img,"Mars",(380,280),cv2.FONT_HERSHEY_COMPLEX,0.5,(255,255,255))
+cv2.putText(img,"Jupiter",(480,350),cv2.FONT_HERSHEY_COMPLEX,0.5,(255,255,255))
+cv2.putText(img,"Saturn",(750,320),cv2.FONT_HERSHEY_COMPLEX,0.5,(255,255,255))
+cv2.putText(img,"Uranus",(950,300),cv2.FONT_HERSHEY_COMPLEX,0.5,(255,255,255))
+cv2.putText(img,"Mars",(1120,300),cv2.FONT_HERSHEY_COMPLEX,0.5,(255,255,255))
+cv2.imshow("output",img)
+cv2.waitKey(0)
